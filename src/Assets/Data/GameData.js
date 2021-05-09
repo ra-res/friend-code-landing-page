29a40196ff1cb1d5925f208ac591b410f62e68eb
ra-res <ra-res@users.noreply.github.com>
2021-05-09 18:31:04 +0100

    Data entered

---
+++ src/Assets/Data/GameData.js
@@ -0,0 +1,57 @@
+games = [
+  {
+    name: "Mario Kart Tour",
+    text:
+      "Mario Kart Tour provides the player with a lot of content. Although mainly inspired by episodes from the Mario Kart series, it introduces new elements, namely certain characters, circuits and karts. The elements are added through regular updates, called seasons",
+  },
+  {
+    name: "Pokemon Go",
+    text:
+      "Pokémon Go is a freemium-type mobile video game based on massively multiplayer localization using augmented reality. The project is jointly created by The Pokémon Company and Niantic, responsible for the Ingress Augmented Reality mobile video game. The game has been available since July 2016 on the iOS and Android platforms. As with the video game series, the goal is to capture Pokémon.",
+  },
+  {
+    name: "AppNana",
+    text:
+      "Swap your AppNana friend code to get gane cards credits. Get Any Paid App For Free. Members Earned Over $10 Million in Games Gift Card Credits. Get any paid app and game for free. Redeem Amazon, iTunes, Google Play and Xbox gift cards. Get AppNana for your iOS or Android. Earn credits with awesome apps and games you play each time.",
+  },
+  {
+    name: "Board Kings",
+    text:
+      "Swap your Board Kings friend code! Join your friends and family in our brand new mingle-play board game experience! The dice will determine where you go! You decide what to do with your coins to build the greatest city for your bunnies! To protect your city from strangers and other unwelcomed mischiefs you need to land on the police station! Upgrade the police station to deploy more police cars!",
+  },
+  {
+    name: "My Singing Monsters",
+    text:
+      "Swap your Singing Monsters friend code! Breed your Monsters to create charming new species. With so many singing Monsters to collect and more Monsters added all the time, you’ll never run out of quirky characters. Mix and match your favorite Singing Monsters to create unique musical melodies! Each Monster has its own unique sound, from booming percussion to cheerful vocals — you’ll be singing along before long!",
+  },
+  {
+    name: "Pokemon Go",
+    text:
+      "Pokémon Go is a freemium-type mobile video game based on massively multiplayer localization using augmented reality. The project is jointly created by The Pokémon Company and Niantic, responsible for the Ingress Augmented Reality mobile video game. The game has been available since July 2016 on the iOS and Android platforms. As with the video game series, the goal is to capture Pokémon.",
+  },
+  {
+    name: "Pokémon Master EX",
+    text:
+      "Trainers partner up with Pokémon to become sync pairs, forming bonds of friendship to carry them to victory! Find new friends on Pasio island. ",
+  },
+  {
+    name: "Call of Duty Mobile",
+    text:
+      "Get new Friends in Call of Duty Mobile and get a lot rewards! All the intensity of Call of Duty at your fingertips. Play as the heroes of Call of Duty: Black Ops and Modern Warfare. Play for free now",
+  },
+  {
+    name: "Animal Crossing New Horizons",
+    text:
+      "You can't make your island a paradise without anyone to share it with! Even if you are not numerous at the beginning, the development of the island will be accompanied by the arrival of new residents. Help them settle in, provide them with services, offer gifts, arrange photo shoots on Photopia Island, and more - all roads lead to friendship! ",
+  },
+  {
+    name: "Nintendo Switch",
+    text:
+      "The Nintendo Switch is a video game console produced by Nintendo, the successor to the Wii U. It is the first hybrid console on the market7 that can be used as a home console as well as a handheld console. Announced on March 17, 2015 at a press conference, officially presented on October 20, 2016 and shown in more detail on January 17, 2017 at a live presentation, it was released worldwide on March 3, 2017. ",
+  },
+  {
+    name: "Pokemon Sword and Pokemon Shield",
+    text:
+      " In the region of Galar there is a vast natural area called 'Terres Sauvages' (Wild Lands). The Wild Lands connect the towns of Galar, and nature stretches as far as the eye can see.",
+  },
+];
